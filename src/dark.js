f79ddb038651c427a82517e9e01fd319cf13d024
Alice Q. Wong <alice@valor.me>
2020-02-19 16:39:16 -0800

    add dark

---
+++ src/dark.js
@@ -0,0 +1,33 @@
+const host = "https://sample-todo.builtwithdark.com";
+
+class Dark {
+
+  /* Helpers to make API calls cleaner */
+  static get(path, callback) {
+    fetch(`${host}${path}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+    .then(response => response.json())
+    .then((json) => {
+      callback(json);
+    });
+  }
+
+  static post(path, body, callback) {
+    fetch(`${host}${path}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    })
+    .then(response => response.json())
+    .then((json) => {
+      callback(json);
+    });
+  }
+
+}
